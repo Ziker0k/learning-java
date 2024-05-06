4d710d9c95070bd93b99ac4e272b2bea5b5a2dcc
Ziker0k <21022002max@gmail.com>
2024-05-06 21:35:26 +0300

    Add Shildt practice directory

---
+++ ShildtPractice/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,10 @@
+package appstart.mymodappdemo;
+import appfuncs.simplefuncs.SimpleMathFuncs;
+public class MyModAppDemo {
+    public static void main(String[] args) {
+        if(SimpleMathFuncs.isFactor(2,10))
+            System.out.println("2 - делитель 10");
+        System.out.println(SimpleMathFuncs.lcf(35, 105));
+        System.out.println(SimpleMathFuncs.gcf(35, 105));
+    }
+}
