a7b106738a33ec8d547fe21e4308fc5b6afc2068
Ziker0k <21022002max@gmail.com>
2024-05-07 04:36:34 +0300

    Add practice with generics.

---
+++ CourseDmdevJavaCore/_6-Generics/src/com/ziker0k/oop/lesson18/hw/PairUtil.java
@@ -0,0 +1,23 @@
+package com.ziker0k.oop.lesson18.hw;
+
+/**
+ * 2. Создать статический обобщённый метод swap
+ * в утилитном классе PairUtil (утилитный класс – это класс
+ * только со статическими методами, который имеет
+ * private конструктор и помечен ключевым словом final,
+ * чтобы предотвратить создание объектов этого класса,
+ * а также наследоваться от него). Метод должен
+ * принимать объект класса Pair и возвращать
+ * новый объект Pair, в которой элементы поменяны местами.
+ */
+
+public final class PairUtil {
+    private PairUtil() {
+    }
+
+    public static <K, V> Pair<V, K> swap(Pair<K, V> source) {
+        K value1 = source.getValue1();
+        V value2 = source.getValue2();
+        return new Pair<V, K>(value2, value1);
+    }
+}
